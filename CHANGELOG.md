3c92e8f94b4bf4848686b0ca1f1a81e0c994c50c
Robert Schütte <schuetterobert@gmail.com>
2025-01-11 20:58:00 +0100

    Updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 # Changelog
 
 ## 0.1.0
+- Simplyfied benchmark for better testing
+- Updated dependencies
 - Initial fork from [html-parser](https://github.com/mathiversen/html-parser)
--- benches/bench_wikipedia.rs
+++ benches/bench_wikipedia.rs
@@ -1,12 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use lithtml::Dom;
 
-static HTML: &'static str = include_str!("./wikipedia-2020-12-21.html");
+static HTML: &'static str = include_str!("./wikipedia-simple.html");
 
 fn wikipedia(c: &mut Criterion) {
     let mut group = c.benchmark_group("wikipedia");
-    group.sample_size(10);
-    group.bench_function("wikipedia", |b| b.iter(|| Dom::parse(HTML).unwrap()));
+    group.bench_function("simple", |b| b.iter(|| Dom::parse(HTML).unwrap()));
     group.finish();
 }
 
